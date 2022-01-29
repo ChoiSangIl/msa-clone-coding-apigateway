5c567c92d0d45648b10e8e32f0ee385271198837
ChoiSangIl <wo566@naver.com>
2022-01-30 00:07:58 +0900

    first commit

---
+++ src/main/java/msa/study/MsaCloneCodingApigatewayApplication.java
@@ -0,0 +1,13 @@
+package msa.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCloneCodingApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaCloneCodingApigatewayApplication.class, args);
+	}
+
+}
